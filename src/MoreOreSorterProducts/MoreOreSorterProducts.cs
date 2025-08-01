e33225b712320068b3c1d91559d1f8e02ab48746
Piotr J <pjackowski18@gmail.com>
2025-08-01 00:31:18 -0500

    initial commit with working mod

---
+++ src/MoreOreSorterProducts/MoreOreSorterProducts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using Mafi;
+using Mafi.Base;
+using Mafi.Core;
+using Mafi.Core.Mods;
+using HarmonyLib;
+namespace MoreOreSorterProducts;
+
+public sealed class MoreOreSorterProducts : DataOnlyMod {
+
+	// Name of this mod. It will be eventually shown to the player.
+	public override string Name => "MoreOreSorterProducts mod";
+
+	// Version, currently unused.
+	public override int Version => 1;
+
+
+	// Mod constructor that lists mod dependencies as parameters.
+	// This guarantee that all listed mods will be loaded before this mod.
+	// It is a good idea to depend on both `Mafi.Core.CoreMod` and `Mafi.Base.BaseMod`.
+	public MoreOreSorterProducts(CoreMod coreMod, BaseMod baseMod) {
+		// You can use Log class for logging. These will be written to the log file
+		// and can be also displayed in the in-game console with command `also_log_to_console`.
+		Log.Info("MoreOreSorterProducts: constructed");
+		var harmony = new Harmony("coi-harmony");
+        harmony.PatchAll();
+	}
+
+
+	public override void RegisterPrototypes(ProtoRegistrator registrator) {
+
+	}
+
+}
---
+++ src/MoreOreSorterProducts/MoreProducts.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Mafi.Collections;
+using Mafi.Core.Products;
+using Mafi.Core.Vehicles;
+using Mafi;
+using HarmonyLib;
+using Mafi.Core.Buildings.OreSorting;
+
+using System.Reflection.Emit;
+
+namespace ExampleMod
+{
+    [HarmonyPatch(typeof(VehicleCargo), nameof(VehicleCargo.CanAdd))]
+    public class MyPatch
+    {
+        // private readonly Lyst<KeyValuePair<ProductProto, Quantity>> m_data;
+        static TypedFieldInfo<VehicleCargo, Lyst<KeyValuePair<ProductProto, Quantity>>> __m_data;
+
+        static MyPatch()
+        {
+            __m_data = new("m_data");
+        }
+
+        static void Postfix(VehicleCargo __instance, ProductProto product, ref bool __result)
+        {
+            var this__m_data = __m_data.GetValue(__instance);
+            __result = this__m_data.Count == 0
+                || __instance.HasProduct(product)
+                || (this__m_data.Count != 20 && this__m_data[0].Key.IsMixable && product.IsMixable);
+        }
+    }
+
+    [HarmonyPatch("Mafi.Core.Vehicles.Excavators.PartialMinedProductTracker", "MaxAllowedQuantityOf")]
+    public class MyPatch2
+    {
+        static FieldInfo __m_minedProducts;
+        static FieldInfo __m_maxCapacity;
+        static FieldInfo __m_usedCapacity;
+
+        static MyPatch2()
+        {
+            __m_minedProducts = typeof(Mafi.Core.TruckCaps).Assembly.GetType("Mafi.Core.Vehicles.Excavators.PartialMinedProductTracker")
+                .GetField("m_minedProducts", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            __m_maxCapacity = typeof(Mafi.Core.TruckCaps).Assembly.GetType("Mafi.Core.Vehicles.Excavators.PartialMinedProductTracker")
+                .GetField("m_maxCapacity", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            __m_usedCapacity = typeof(Mafi.Core.TruckCaps).Assembly.GetType("Mafi.Core.Vehicles.Excavators.PartialMinedProductTracker")
+                .GetField("m_usedCapacity", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        static void Postfix(object __instance, ProductProto product, ref PartialQuantity __result)
+        {
+            var m_minedProducts = (LystStruct<KeyValuePair<ProductProto, PartialQuantity>>)__m_minedProducts.GetValue(__instance);
+            var m_maxCapacity = (Quantity)__m_maxCapacity.GetValue(__instance);
+            var m_usedCapacity = (Quantity)__m_usedCapacity.GetValue(__instance);
+
+            if (m_minedProducts.ContainsKey(product))
+            {
+                __result = (m_maxCapacity - m_usedCapacity).AsPartial - m_minedProducts.GetValueOrDefault(product).FractionalPart;
+                return;
+            }
+
+            if (m_minedProducts.Count >= 20)
+            {
+                __result = PartialQuantity.Zero;
+                return;
+            }
+
+            __result = (m_maxCapacity - m_usedCapacity).AsPartial;
+        }
+    }
+
+    [HarmonyPatch(typeof(OreSortingPlant), nameof(OreSortingPlant.AddProductToSort))]
+    public class MyPatch3
+    {
+
+        static MyPatch3()
+        {
+
+        }
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+            
+            var codes = new List<CodeInstruction>(instructions);
+            //Log.Info("CodeInfo:" + codes + "\n size: " + codes.Count);
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+                //Log.Info("Code:" + codes[i].opcode + "\nOperand:" + codes[i].operand);
+
+                if (codes[i].opcode == OpCodes.Ldc_I4_8)
+                {
+                    //Log.Info("Found code:" + i);
+                   
+                    codes[i].opcode = OpCodes.Ldc_I4_S;
+                    codes[i].operand = 20;
+
+                }
+            }
+
+            return codes.AsEnumerable();
+        }
+     
+    }
+
+    [HarmonyPatch]
+    static class Patch
+    {
+        public static MethodBase TargetMethod()
+        {
+            Type innerClass = AccessTools.Inner(AccessTools.TypeByName("Mafi.Unity.Ui.Inspectors.OreSortingPlantInspector"), "<>c__DisplayClass0_0");
+            return AccessTools.Method(innerClass, "<.ctor>b__16");
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+
+                if (codes[i].opcode == OpCodes.Ldc_I4_8)
+                {
+                    //Log.Info("Found code:" + i);
+                    //index = i;
+                    codes[i].opcode = OpCodes.Ldc_I4_S;
+                    codes[i].operand = 20;
+
+                }
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+
+    class TypedFieldInfo<TInstance, TResult>
+    {
+        FieldInfo _info;
+        public TypedFieldInfo(string fieldName) => _info = typeof(TInstance).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+        public TResult GetValue(TInstance obj) => (TResult)_info.GetValue(obj);
+    }
+}
